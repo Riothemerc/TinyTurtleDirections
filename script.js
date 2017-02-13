9cb0a94ee0c21e14d84c7805bdaf5609be010ca4
Isaiah Pierre Saint <riothemerc@gmail.com>
2017-02-13 15:32:25 +0000

    tinyturtle continued

---
+++ script.js
@@ -0,0 +1,24 @@
+ TinyTurtle.apply(window); 
+ 
+ 
+//  stamp();
+ 
+//  right(20);
+//  forward(50);
+//  forward(48);
+//  left(70);
+ 
+ 
+ right(90);
+ forward(50);
+ right(90);
+ forward(50);
+ right(90);
+ forward(50);
+ right(90);
+ forward(50);
+ 
+ right(30);
+ forward(50);
+ right(120);
+ forward(50);
